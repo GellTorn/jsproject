8324e0cbe0d348fd8c8c7c8d9d7ac63030686734
Yury Petrov <yuratorn@gmail.com>
2020-12-05 19:03:57 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,6 @@
 const path = require('path');
 
 module.exports = [
-  // {
-  //   entry: __dirname + '/dist/Game.js',
-  //   output: {
-  //     path: __dirname + '/dist',
-  //     filename: 'main.js',
-  //   },
-  //   devServer: {  // configuration for webpack-dev-server
-  //     contentBase: '.',  //source of static assets
-  //     port: 80, // port to run dev-server
-  //   } 
-  // },
-  // {
-  //   entry: __dirname + '/dist/demo1.js',
-  //   output: {
-  //     path: __dirname + '/dist',
-  //     filename: 'demo1.js',
-  //   }
-  // }
   {
     entry: path.resolve(__dirname, 'dist/src/Game.js'),
     output: {
